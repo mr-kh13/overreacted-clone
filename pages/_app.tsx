d2e817ed522feb6145516f58e9c7deaeb1e87c59
mohammad <mohammad.kh06@gmail.com>
2022-03-19 19:52:25 +0330

    add title to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import type { AppProps } from "next/app";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "@/store";
 import { ThemeProvider } from "@/providers/ThemeProvider";
+import { appName } from "@/constants/appConfigs";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ReduxProvider store={store}>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ReduxProvider>
+    <>
+      <title>{appName}</title>
+      <ReduxProvider store={store}>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ReduxProvider>
+    </>
   );
 }
