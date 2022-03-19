2860290f45f9ff7cbf32c2699a1ab657d5ce07c2
mohammad <mohammad.kh06@gmail.com>
2022-03-19 17:15:05 +0330

    create AppContainer

---
+++ src/components/layout/AppContainer.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div({
+  marginLeft: "auto",
+  marginRight: "auto",
+  maxWidth: "42rem",
+  padding: "2.625rem 1.3125rem",
+});
